7a9f25ed1005846ca3c2b92ed50bf62d9a8fd8c3
Albert <albert.bayazidi@gmail.com>
2024-05-22 08:57:57 +0200

    updated the text and resized the div to make the page fit all at once

--- src/Components/info.jsx
+++ src/Components/info.jsx
@@ -3,9 +3,10 @@ import { Fragment } from "react"
 function Info() {
   return (
     <Fragment>
-    <div class = 'p-10 mt-5 mx-10 bg-primary rounded-xl'>
+    <div class = 'py-5 px-10 mt-2 mx-10 bg-primary rounded-xl'>
         <h1 class = 'text-white text-center text-xl pb-5'>Info</h1>
-        <p class = 'text-white text-center'> Denne nettsiden prøver å finne de billigeste bilettprisene pr dag i en månded og framstiller dem i en priskalender.</p>
+        <p class = 'text-white text-center'> 
+        Denne nettsiden prøver å finne de billigeste bilettprisene en månded fram i tid.</p>
     </div>
     <img src='/footer_art.png' />
     </Fragment>
