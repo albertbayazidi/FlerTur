24ae66e1c160d19a1d24f52f186a1f3943af276f
Albert <albert.bayazidi@gmail.com>
2024-05-22 18:22:05 +0200

    refactored code with prettier

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,38 +1,46 @@
-import '../App.css'
+import "../App.css";
 
-function SearchBarArea(){
-
-    return (
-        <div class = 'bg-second'>
-            <div class = 'bg-second py-8 px-10 grid justify-items-center grid-cols-3' >
-                <div> 
-                    <input class = 'py-2 px-1 rounded text-primary focus:outline-none' type='text' placeholder='Fra' list='cityname'/>
-                    <datalist id='cityname'>
-                        <option value='Oslo S'/>
-                        <option value='Trondheim S'/>
-                        <option value='Lillehammer stasjon'/>
-                        <option value='Stavanger stasjon'/>
-                        <option value='Bergen stasjon'/>
-                        <option value='Lillestrøm stasjon'/>
-                        <option value='Fredrikstad stasjon'/>
-                        <option value='Kristiansand stasjon'/>
-                        <option value='Kongsvinger stasjon'/>
-                    </datalist>   
-                </div>
-                <div>
-                    <button type='button' class=' bg-primary  hover:bg-second hover:ring hover:ring-primary rounded text-white p-2'>
-                    Bytt retning               
-                    </button>
-                </div>
-
-                <div >
-                    <input class = 'py-2 px-1 rounded text-primary focus:outline-none' type='text' placeholder='Til' list='cityname'/>
-                </div>
-            </div>
-            <img src='/search_bar_kunst_remix.png'/>
+function SearchBarArea() {
+  return (
+    <div class="bg-second">
+      <div class="grid grid-cols-3 justify-items-center bg-second px-10 py-8">
+        <div>
+          <input
+            class="rounded px-1 py-2 text-primary focus:outline-none"
+            type="text"
+            placeholder="Fra"
+            list="cityname"/>
+          <datalist id="cityname">
+            <option value="Oslo S" />
+            <option value="Trondheim S" />
+            <option value="Lillehammer stasjon" />
+            <option value="Stavanger stasjon" />
+            <option value="Bergen stasjon" />
+            <option value="Lillestrøm stasjon" />
+            <option value="Fredrikstad stasjon" />
+            <option value="Kristiansand stasjon" />
+            <option value="Kongsvinger stasjon" />
+          </datalist>
         </div>
-    )
+        <div>
+          <button
+            type="button"
+            class=" rounded  bg-primary p-2 text-white hover:bg-second hover:ring hover:ring-primary">
+            Bytt retning
+          </button>
+        </div>
 
+        <div>
+          <input
+            class="rounded px-1 py-2 text-primary focus:outline-none"
+            type="text"
+            placeholder="Til"
+            list="cityname"/>
+        </div>
+      </div>
+      <img src="/search_bar_kunst_remix.png" />
+    </div>
+  );
 }
 
 export default SearchBarArea;
--- src/Components/info.jsx
+++ src/Components/info.jsx
@@ -1,14 +1,16 @@
-import { Fragment } from "react"
+import { Fragment } from "react";
 
 function Info() {
   return (
     <Fragment>
-    <div class = 'py-5 px-10 mt-2 mx-10 bg-primary rounded-xl'>
-        <h1 class = 'text-white text-center text-xl pb-5'>Info</h1>
-        <p class = 'text-white text-center'> 
-        Denne nettsiden prøver å finne de billigeste bilettprisene en månded fram i tid.</p>
-    </div>
-    <img src='/footer_art.png' />
+      <div class="mx-10 mt-2 rounded-xl bg-primary px-10 py-5">
+        <h1 class="pb-5 text-center text-xl text-white">Info</h1>
+        <p class="text-center text-white">
+          Denne nettsiden prøver å finne de billigeste bilettprisene en månded
+          fram i tid.
+        </p>
+      </div>
+      <img src="/footer_art.png" />
     </Fragment>
   );
 }
