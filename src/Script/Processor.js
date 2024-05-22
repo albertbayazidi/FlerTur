5223333ad7369804e11de8c6835d9d87fb55f464
Albert <albert.bayazidi@gmail.com>
2024-05-22 09:01:41 +0200

    added kongsvinger to the list, made function async since we need await for the scrapedata

--- src/Script/Processor.js
+++ src/Script/Processor.js
@@ -1,9 +1,6 @@
 import { ScrapeData } from './Scraper.js';
 
 
-//ScrapeData('https://entur.no/reiseresultater?transportModes=rail%2Ctram%2Cbus%2Ccoach%2Cwater%2Ccar_ferry%2Cmetro%2Cflytog%2Cflybuss&date=1717725600000&tripMode=oneway&walkSpeed=1.3&minimumTransferTime=120&timepickerMode=departAfter&allowFlexible=false&startId=NSR%3AStopPlace%3A420&startLabel=Lillehammer%20stasjon&startLat=61.114912&startLon=10.461479&stopId=NSR%3AStopPlace%3A59872&stopLabel=Oslo%20S&stopLat=59.910357&stopLon=10.753051')
-//.then(data => console.log(data), err => console.error(err));
-
 const stations = new Map([
     ["Oslo%20S", ["3A59872", "59.910357", "10.753051"]],
     ["Trondheim%20S", ["3A59977", "63.436279", "10.399123"]],
@@ -12,11 +9,12 @@ const stations = new Map([
     ["Bergen%20stasjon", ["3A59983", "60.390434", "5.333511"]],
     ["Lillestrøm%20stasjon", ["3A62339", "59.952915", "11.045364&"]],
     ["Fredrikstad%20stasjon", ["3A58794", "59.208805", "10.950282"]],
-    ["Kristiansand%20stasjon", ["3A61608", "58.14559", "7.988067"]]
+    ["Kristiansand%20stasjon", ["3A61608", "58.14559", "7.988067"]],
+    ["Kongsvinger%20stasjon", ["3A58808", "60.187497", "12.004063"]],
 ]);
 
 
-function process(start_loc,end_loc){
+async function process(start_loc,end_loc){
     const days = 30;
 
     function addDaysToDate(date) {
@@ -52,10 +50,7 @@ function process(start_loc,end_loc){
 
     const times = [];
 
-    console.log('updated day', new_date);
-    console.log('days between',diff_in_day);
-
-    for (let i = days-1; i > 0; i--){
+    for (let i = days; i > 0; i--){
         times.push(convertDateToUrl(diff_in_day - i));
     }
 
@@ -67,6 +62,8 @@ function process(start_loc,end_loc){
 
     let stop_id = 'stopId=NSR%3AStopPlace%';
 
+    let storage = [];
+
     for (let i = 0; i < times.length; i++){
         let url_kopi = base_url;
         url_kopi += times[i] + '&' + trip_mode;
@@ -75,9 +72,11 @@ function process(start_loc,end_loc){
 
         url_kopi += stop_id + stations.get(end_loc)[0] + '&stopLabel=' + end_loc + '&stopLat=' + stations.get(end_loc)[1] + '&stopLon=' + stations.get(end_loc)[2];
         var new_url = url_kopi;
-    }
-    console.log(new_url)
 
+        var data = await ScrapeData(new_url);
+        storage.push(data);
+        console.log("time_url", times[i] ,data , i);
+    }
 
 }
 process('Lillehammer%20stasjon','Fredrikstad%20stasjon');
