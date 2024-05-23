03019b6beb1d409eaca2784fdc50b0edc61056fc
Albert <albert.bayazidi@gmail.com>
2024-05-23 03:46:03 +0200

    linked main page to website

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -17,14 +17,14 @@ function Navbar() {
                 class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 id="user-menu-button"
                 aria-expanded="false"
-                aria-haspopup="true"
-              >
-                <span class="absolute -inset-1.5"></span>
+                aria-haspopup="true">
+                <a href="https://bayazidi.xyz"  target="_blank" >
                 <img
                   class="h-8 w-8 rounded-full"
                   src="https://bayazidi.xyz/pb.jpeg"
-                  alt=""
+                  alt="profile image"
                 />
+                </a>
               </button>
             </div>
           </div>
